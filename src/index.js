d76b203923363f0d6f65a5a9ecd428e686ee8478
Maniyavel <116820894+Maniyavel@users.noreply.github.com>
2023-05-03 22:49:17 +0300

    added styles

--- src/index.js
+++ src/index.js
@@ -1,68 +1,116 @@
+import './css/styles.css';
 import axios from 'axios';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-
+const button = document.querySelector('.load-more');
 const form=document.querySelector('#search-form');
 const gallery=document.querySelector('.gallery');
+
 const per_page=40;
-let totalPages = 0;
-let nameImages = '';
+let query = 0;
 let page=1;
+
+
+function createMarkup(images){
+  return images
+  .map((
+      {webformatURL,
+      largeImageURL,
+      tags,
+      likes,
+      views,
+      comments,
+      downloads,
+      }) =>`<div class="photo-card">
+      <a class="photo-link" href="${largeImageURL}>
+      <img class="gallery-img" src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+      <div class="info">
+        <p class="info-item">
+          <b>Likes</b>${likes}
+        </p>
+        <p class="info-item">
+          <b>Views</b>${views}
+        </p>
+        <p class="info-item">
+          <b>Comments</b>${comments}
+        </p>
+        <p class="info-item">
+          <b>Downloads</b>${downloads}
+        </p>
+      </div>
+    </div>`).join('');
+  gallery.insertAdjacentHTML('beforeend', images);
+  simpleLightbox.refresh();
+}
+
+const simpleLightbox = new SimpleLightbox('.gallery a ', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 
 form.addEventListener('submit', onSubmit);
 
 async function onSubmit(e){
   e.preventDefault();
- 
+  page=1;
+  button.style.display = 'none';
+  gallery.innerHTML = '';
+  const elements = form.elements;
+  query = elements.searchQuery.value;
+  const images = await getImages(query, page, per_page);
+
+  if (images.hits.length === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+
+    return false;
+
 }
 
-async function getImages(nameImages, page){
+createMarkup(images.hits);
+  button.style.display = 'block';
+  page = page + 1;
+}
+
+button.addEventListener('click', onClick);
+
+async function onClick(e){
+  button.style.display = 'none';
+  const images=getImages(query, page, per_page);
+  createMarkup(images,hits);
+  button.style.display = 'block';
+
+  page = page + 1;
+  if ((page - 1) * per_page >= images.totalHits) {
+    Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+    button.style.display = 'none';
+  }
+}
+
+
+async function getImages(query, page, per_page){
     const API_KEY='35973016-00fa5ed31471a0bdacb65a24f';
     
    
         const params=new URLSearchParams({
             key: API_KEY,
-            q: nameImages,
+            q: query,
             image_type: 'photo',
-            orientation: 'horizontal',
+            orientation: 'horizontal', 
             safesearch: true,
             per_page: per_page,
             page: page,
         })
 
         const respons=await axios.get(`https://pixabay.com/api/?${params}`);
-        totalPages = response.data.totalHits / per_page;
-        return respons;
+        
+        return respons.data;
     }
 
 
-function createMarkup(images){
-    return images
-    .map((
-        {webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-        }) =>`<div class="photo-card">
-        <img class="gallery-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
-        <div class="info">
-          <p class="info-item">
-            <b>Likes</b>${likes}
-          </p>
-          <p class="info-item">
-            <b>Views</b>${views}
-          </p>
-          <p class="info-item">
-            <b>Comments</b>${comments}
-          </p>
-          <p class="info-item">
-            <b>Downloads</b>${downloads}
-          </p>
-        </div>
-      </div>`).join('');
-    gallery.insertAdjacentHTML('beforeend', images);
-}
